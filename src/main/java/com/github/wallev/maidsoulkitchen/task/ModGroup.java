23d7afdbe24fe9300d548c0a98c6ce00592bf810
Azumic <775300932@qq.com>
2025-06-26 23:54:33 +0800

    补充modgroup黑名单，便于同步

---
+++ src/main/java/com/github/wallev/maidsoulkitchen/task/ModGroup.java
@@ -0,0 +1,43 @@
+package com.github.wallev.maidsoulkitchen.task;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+public enum ModGroup {
+    NONE(),
+
+    BLACK("com.github.wallev",
+            "com.github.tartaricacid",
+            "java.util",
+            "java.lang",
+            "com.google",
+            "org.apache",
+            "net.minecraft",
+            "net.minecraftforge",
+            "net.neoforged",
+            "com.mojang"),
+
+    XKMC("dev.xkmc"),
+    SAMMY("com.sammy"),
+    VECTORWING("vectorwing"),
+    TEAM_TEA("com.teamtea"),
+    SERENESEASONS("sereneseasons"),
+    MAO("com.mao"),
+    UMPAZ("umpaz"),
+    SIHENZHANG("com.sihenzhang"),
+    LEKAVAR_LMA("lekavar.lma"),
+    YSBBBBBB("com.github.ysbbbbbb"),
+    TT_432("io.github.tt432"),
+
+    ;
+    public final List<String> groups;
+
+    ModGroup(String... groups) {
+        this.groups = Lists.newArrayList(groups);
+    }
+
+    public static ModGroup by(String key) {
+        return ModGroup.valueOf(key);
+    }
+}
